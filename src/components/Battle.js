225da85a8b00a0fdf3dceeb139ae54ce5754e10d
lasjorg <28780271+lasjorg@users.noreply.github.com>
2018-11-09 12:09:13 +0100

    Refactor to use class properties

--- src/components/Battle.js
+++ src/components/Battle.js
@@ -4,24 +4,29 @@ import { Link } from 'react-router-dom';
 import PlayerPreview from './PlayerPreview';
 
 class PlayerInput extends Component {
-  constructor(props) {
-    super(props);
 
-    this.state = {
-      username: ''
-    };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+  static propTypes = {
+    id: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    onSubmit: PropTypes.func.isRequired
   }
 
-  handleChange(event) {
+  static defaultProps = {
+    label: 'UserName'
+  }
+
+  state = {
+    username: ''
+  }
+
+  handleChange = (event) => {
     let value = event.target.value;
     this.setState({
       username: value
     });
   }
 
-  handleSubmit(event) {
+  handleSubmit = (event) => {
     event.preventDefault();
 
     this.props.onSubmit(this.props.id, this.state.username);
@@ -51,34 +56,22 @@ class PlayerInput extends Component {
   }
 }
 
-PlayerInput.propTypes = {
-  id: PropTypes.string.isRequired,
-  label: PropTypes.string.isRequired,
-  onSubmit: PropTypes.func.isRequired
-};
-
 class Battle extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
+    state = {
       playerOneName: '',
       playerOneImage: null,
       playerTwoName: '',
       playerTwoImage: null
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
-    this.handleReset = this.handleReset.bind(this);
-  }
 
-  handleSubmit(id, username) {
+  handleSubmit = (id, username) => {
     this.setState(() => ({
       [id + 'Name']: username,
       [id + 'Image']: `https://github.com/${username}.png?size=200`
     }));
   }
 
-  handleReset(id) {
+  handleReset = (id) => {
     this.setState(() => ({
       [id + 'Name']: '',
       [id + 'Image']: null
